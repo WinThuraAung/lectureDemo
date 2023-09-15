d9f9afee062322bf15815ed861574cf5d26abd0d
WinThuraAung <Ronaldwu5@gmail.com>
2023-09-15 16:42:35 -0700

    Added "Welcome"

--- lectureDemo/ContentView.swift
+++ lectureDemo/ContentView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             Text("你好")
+            Text("welcome")
 
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            
+             
         }
         .padding()
         
