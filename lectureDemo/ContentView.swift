6debe5ca43f66aa4c19273d9bbeca8c5ac1506c3
Ronald Aung <Winthuraaung@berkeley.edu>
2023-09-14 19:26:00 -0700

    Initial Commit

---
+++ lectureDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  lectureDemo
+//
+//  Created by Ronald on 14/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("你好")
+
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            
+        }
+        .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lectureDemo/lectureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  lectureDemoApp.swift
+//  lectureDemo
+//
+//  Created by Ronald on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct lectureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
